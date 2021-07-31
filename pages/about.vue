d37052ca881f222786ab30e02e12eeb07e729cfb
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-07-31 16:09:58 +0900

    [master] add two buttons

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="text-xl font-bold">About</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: '',
+  setup() {},
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/works.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mx-auto mt-4">
+    <h1 class="text-xl font-bold">Works</h1>
+    <button
+      class="
+        bg-blue-500
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded
+      "
+    >
+      Click
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: '',
+  setup() {},
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
